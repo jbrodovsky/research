21d2c5f895b2fe248a2b4c81a50cf54d4d4838f8
James Brodovsky <57160841+jbrodovsky@users.noreply.github.com>
2023-10-16 11:24:45 -0400

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -4,7 +4,7 @@
  # Do not use this in production. Sample purpose only.
  # Author: Upkar Lidder (IBM)
  # Additional Authors:
- # <your GitHub username>
+ # James Brodovsky
  # Input:
  # p, principal amount
  # t, time period in years
